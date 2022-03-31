24f94a314b785655b6274e189572215d01b62255
User <User@DESKTOP-J3GT9R3>
2022-03-31 09:46:53 +0100

    project initialisaion 2

--- src/gestions/Gestions.java
+++ src/gestions/Gestions.java
@@ -16,7 +16,7 @@ public class Gestions {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println();
+        System.out.println("Hello");
     }
     
 }
